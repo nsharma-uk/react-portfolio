e4a58c1c0b4c09043b29bb08c58c6cefea1414ae
nsharma-uk <n.sharmauk711@gmail.com>
2022-09-02 16:37:10 +0100

    style changes on about me page

--- src/components/About.js
+++ src/components/About.js
@@ -3,14 +3,16 @@ import Container from "@mui/material/Container";
 import me from "../Assets/images/avatar.jpg";
 import useMediaQuery from "@mui/material/useMediaQuery";
 import Avatar from "@mui/material/Avatar";
+import Stack from "@mui/material/Stack";
 
-import Box from "@mui/material/Box";
+//import Box from "@mui/material/Box";
 export const About = () => {
   const isMobile = useMediaQuery("(max-width:600px)");
   return (
-    <Box
+    <Stack
       sx={{
         backgroundColor: "#75c9b7",
+        direction: "column",
         width: "100%",
         height: "100vh",
         display: "flex",
@@ -24,17 +26,25 @@ export const About = () => {
         sx={{
           width: isMobile ? 190 : 300,
           height: isMobile ? 190 : 300,
-          margin: 10,
+          marginTop: 8,
+          marginBottom: 4,
         }}
       />
-      <Container>
-        <Typography variant="h5" gutterBottom>
-          Hi, I'm Nayan and I'm a full stack developer. I am a former teacher
-          who wanted to pursue a new, fresh challenge, specifically a career in
-          web development. My technical skill set include MongoDB, Express.js,
-          React, Node.js, HTML5, CSS3, Javascript, MySQL and ApolloDB.
-        </Typography>
-      </Container>
-    </Box>
+
+      <Typography
+        variant="h6"
+        gutterBottom
+        sx={{
+          marginBottom: 4,
+          paddingLeft: 6,
+          paddingRight: 6,
+        }}
+      >
+        Hi, I'm Nayan and I'm a full stack developer. I am a former teacher who
+        wanted to pursue a new, fresh challenge, specifically a career in web
+        development. My technical skill set include MongoDB, Express.js, React,
+        Node.js, HTML5, CSS3, Javascript, MySQL and ApolloDB.
+      </Typography>
+    </Stack>
   );
 };
